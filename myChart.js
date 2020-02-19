07aec5d19a3af85e9b7bc5247e6a4b2aaf3e804a
aditiusf <59901287+aditiusf@users.noreply.github.com>
2020-02-18 21:22:37 -0500

    Add files via upload

---
+++ myChart.js
@@ -0,0 +1,21 @@
+function renderChart(data, labels) {
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'This week',
+                data: data,
+            }]
+        },
+    });
+}
+
+$("#renderBtn").click(
+    function () {
+        data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
+        labels =  ["Apples", "Bananas", "Grapes", "Oranges", "Peach", "Pineapple", "Plum"];
+        renderChart(data, labels);
+    }
+);
